917bf667eaada2c0cb299f9d52e729749623d263
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2024-07-24 18:09:25 +0300

    Update calculateAverage.js

--- src/components/Estimate/calculateAverage.js
+++ src/components/Estimate/calculateAverage.js
@@ -1,5 +1,9 @@
 export function calculateAverage(arr) {
-    const sum = arr.reduce((acc, num) => acc + num, 0);
-        const avg = sum / arr.length;
+    const numbers = arr
+        .map(num => parseFloat(num))
+        .filter(num => !isNaN(num));
+
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+        const avg = sum / numbers.length;
         return avg;
 }
